98bf0fbb115090707c1af823fd9025ed72952719
Fabian Lippold <fabianlippold1184@gmail.com>
2026-09-08 22:38:30 +0200

    Restore original post-switch portrait blink timing

--- src/legoapi/render/core/render.cpp
+++ src/legoapi/render/core/render.cpp
@@ -3051,8 +3051,7 @@ void DrawPanel() {
                 const f32 alpha = static_cast<i8>(player->apiobj.flags_low) < 0 ? 1.0f : DROPINALPHA;
                 const i32 character_id = player->field_0xcc0 == NULL ? player->id : player->field_0xcc0->id;
                 const f32 icon_timer = player->hud_icon_timer;
-                const i32 draw_character =
-                    icon_timer <= 0.0f || (icon_timer < 0.5f && NuFmod(icon_timer, 0.4f) < 0.075f);
+                const i32 draw_character = icon_timer <= 0.0f || (icon_timer < 2.0f && NuFmod(icon_timer, 0.4f) < 0.2f);
                 DrawCharIcon(character_id, -ICONX, status_y, 0.0f, ICONSIZE, 0xa6, alpha, alpha, draw_character, NULL);
                 if (static_cast<i8>(player->apiobj.flags_low) < 0 && player->apiobj.character_data != NULL &&
                     player->apiobj.character_data->name_id != -1) {
